fae1f5cf389ce82598226efbedff6b984bd0e6ed
iyj <iyj@qq.com>
2023-01-30 13:44:52 +0800

    b

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to NewV3 <a href="https://nextjs.org">Next.js!</a>
+          Welcome to NewV5 <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
